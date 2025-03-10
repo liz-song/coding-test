642396e294d313b013899f07f9a6f49b07014ff3
eunseo song <liz.snen@gmail.com>
2025-03-10 09:14:53 +0900

    250310 study

---
+++ leetcode/easy/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,10 @@
+﻿/*
+- 적어도 5명의 직원이 있는 매니저의 이름 출력 
+- 셀프 조인해서,  name이 manager ID에 5개 이상 존재하는 사람 찾아야함(having count사용해서) 
+*/
+
+SELECT B.name
+FROM Employee A
+    JOIN Employee B ON A.managerId = B.id
+GROUP BY B.id, B.name
+HAVING COUNT(A.id) >= 5;
