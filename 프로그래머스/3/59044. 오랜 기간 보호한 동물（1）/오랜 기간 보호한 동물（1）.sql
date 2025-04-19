102e593de1bd2fb7ed36a642e93ae80ca7f2be25
Eunseo Song (송은서) <163216908+liz-song@users.noreply.github.com>
2025-04-19 14:40:13 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,15 @@
--- 코드를 입력하세요
-SELECT i.NAME,i.DATETIME
-FROM ANIMAL_INS i
-LEFT JOIN ANIMAL_OUTS o ON i.ANIMAL_ID = o.ANIMAL_ID
-WHERE o.DATETIME IS NULL
-ORDER BY DATETIME
+/*
+- 아직 입양을 못 갔다 = in이랑 out을 left join했는데 out.id가 null이다.
+- 가장 오래 보호소에 있었다 = a.datetime을 오름차순으로 정렬했을 때 limit 3개 
+- in A와 OUT B를 left join해서 WHERE 조건으로 IS NULL 사용 
+*/
+
+SELECT
+    A.NAME,
+    A.DATETIME
+FROM ANIMAL_INS A
+    LEFT JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE B.ANIMAL_ID IS NULL
+ORDER BY A.DATETIME ASC
 LIMIT 3;
+
